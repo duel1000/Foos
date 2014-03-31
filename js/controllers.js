35f2392e961f8ad0414627749406ccb75310dce6
duel1000 <martindue89@hotmail.com>
2014-03-31 22:14:03 +0200

    adsdasd

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,7 @@ var myNameSpace = angular.module('myApp',[]);
 myNameSpace.controller('MyController', function($scope, $http){
 $http.get('js/data.json').success(function(data){
 	$scope.authors = data;
+	}).error(function(){
+		alert("lol");
 	});
 });
